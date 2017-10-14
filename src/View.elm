8a0fffed21ec2e94735a37f355e304fac3325c83
Oliver Sõro <oliver.s6ro@gmail.com>
2017-10-15 01:06:14 +0300

    Remove old, commented out rendering from View.

--- src/View.elm
+++ src/View.elm
@@ -52,7 +52,8 @@ renderCell : Grid -> Cell -> Form
 renderCell g c =
     case c.shape of
         Ball _ ->
-            Cell.size // 2
+            Cell.size
+                // 2
                 |> toFloat
                 |> C.circle
                 |> C.filled c.color
@@ -98,40 +99,3 @@ canvasXY g c =
             - (Cell.size // 2)
             |> toFloat
         )
-
-
-
-{- }
-   render : Model -> Html Msg
-   render m =
-     let
-       cs = m.grid.config.cellSize
-       pw = (m.grid.config.gridWidth * m.grid.config.cellSize)
-       ph = (m.grid.config.gridHeight * m.grid.config.cellSize)
-       toCoords c =
-         ( ((pw // 2) - pw) + (c.cx * cs) + (cs // 2) |> toFloat
-         , ph - (ph // 2) - (c.cy * cs) - (cs // 2) |> toFloat
-         )
-     in
-       m.wsize
-         |> Maybe.map (\ws ->
-           Dict.values m.grid.cells
-             |> List.map (\c ->
-               case c.shape of
-                 Rectangle ->
-                   toFloat cs
-                     |> C.square
-                     |> C.filled c.color
-                     |> C.move (toCoords c)
-                 Circle ->
-                   toFloat cs / 2
-                     |> C.circle
-                     |> C.filled c.color
-                     |> C.move (toCoords c)
-             )
-             |> C.collage pw ph
-             |> E.container ws.width ws.height E.middle
-             |> E.toHtml
-         )
-         |> Maybe.withDefault (text "Waiting for Window size ...")
--}
